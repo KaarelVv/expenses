bc8031c48f97fc702a7b265cdd759c30e872dd57
Kaarel <kaarel.viilvere@voco.ee>
2025-01-10 12:46:44 +0200

    form finish

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,17 +3,19 @@ import "./ExpenseForm.css"
 
 const ExpenseForm = (props) => {
 
-    
-          
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredPrice, setEnteredPrice] = useState('') 
+    const [enteredDate, setEnteredDate] = useState('')  
+      
 
     const titleChangehandler = (event) => {
-        
+        setEnteredTitle(event.target.value)
     }
     const priceChangehandler = (event) => {
-       
+        setEnteredPrice(event.target.value)
     }
     const dateChangehandler = (event) => {
-        
+        setEnteredDate(event.target.value)
     }
 
     const submithandler = (event) => {
@@ -23,6 +25,10 @@ const ExpenseForm = (props) => {
             price: enteredPrice,
             date: new Date(enteredDate)
         }
+        console.log(expenseData)
+        setEnteredTitle('')
+        setEnteredPrice('')
+        setEnteredDate('')
         
     }
     return (
@@ -32,18 +38,21 @@ const ExpenseForm = (props) => {
                     <label>Title</label>
                     <input type="text"
                         onChange={titleChangehandler}
+                        value={setEnteredTitle}
                     />
                 </div>
                 <div className="new-expense_control">
                     <label>Price</label>
                     <input type="number" min="0.01" step="0.01"
                         onChange={priceChangehandler}
+                        value={setEnteredPrice}
                     />
                 </div>
                 <div className="new-expense_control">
                     <label>Date</label>
                     <input type="date" min="2024-11-12" max="2026-01-31"
                         onChange={dateChangehandler}
+                        value={setEnteredDate}
                     />
                 </div>
             </div>
